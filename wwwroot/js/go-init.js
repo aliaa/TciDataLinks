da93b89faa5af12c4f2a1148738e795bf0c682f2
Ali Aboutalebi <aboutalebi@live.com>
2020-05-09 15:17:53 +0430

    graph tooltip show

--- wwwroot/js/go-init.js
+++ wwwroot/js/go-init.js
@@ -57,7 +57,7 @@
                     toolTip:  //  define a tooltip for each link that displays its information
                         gojs("ToolTip",
                             gojs(go.TextBlock, { margin: 4 },
-                                new go.Binding("text", "text", getInfo))
+                                new go.Binding("text", "text"))
                         )
                 }
             );
@@ -91,7 +91,7 @@
                         name: "GROUPTEXT",
                         alignment: go.Spot.TopLeft,
                         alignmentFocus: new go.Spot(0, 0, -4, -4),
-                        font: "Bold 10pt Sans-Serif"
+                        font: "Bold 10pt 'Segoe UI'"
                     },
                     new go.Binding("text", "text")),
                 {
@@ -135,67 +135,8 @@
 
 // returns the text for a tooltip, param obj is the text itself
 function getInfo(model, obj) {
-    var x = obj.panel.adornedPart; // the object that the mouse is over
-    var text = ""; // what will be displayed
-    if (x instanceof go.Node) {
-        if (x instanceof go.Group) text += "Group: "; else text += "Node: ";
-        text += x.data.key;
-        var toLst = nodesTo(x, 0); // display names of nodes going into this node
-        if (toLst.count > 0) {
-            toLst.sort(function (a, b) { return a < b ? -1 : 1 });
-            text += "\nNodes into: ";
-            toLst.each(function (key) {
-                if (key !== text.substring(text.length - 3, text.length - 2)) {
-                    text += key + ", ";
-                }
-            });
-            text = text.substring(0, text.length - 2);
-        }
-        var frLst = nodesFrom(x, 0); // display names of nodes coming out of this node
-        if (frLst.count > 0) {
-            frLst.sort(function (a, b) { return a < b ? -1 : 1 });
-            text += "\nNodes out of: ";
-            frLst.each(function (key) {
-                if (key !== text.substring(text.length - 3, text.length - 2)) {
-                    text += key + ", ";
-                }
-            });
-            text = text.substring(0, text.length - 2);
-        }
-        var grpC = containing(x, 0); // if the node is in a group, display its name
-        if (grpC !== null) text += "\nContaining SubGraph: " + grpC.data.key;
-        if (x instanceof go.Group) {
-            // if it"s a group, also display nodes and links contained in it
-            text += "\nMember nodes: ";
-            var children = childNodes(x, 0);
-            children.sort(function (a, b) { return a < b ? -1 : 1 });
-            children.each(function (key) {
-                if (key !== text.substring(text.length - 3, text.length - 2)) {
-                    text += key + ", ";
-                }
-            });
-            text = text.substring(0, text.length - 2);
-
-            var linkChildren = childLinks(x, 0);
-            if (linkChildren.count > 0) {
-                text += "\nMember links: ";
-                var linkStrings = new go.List(/*"string"*/);
-                linkChildren.each(function (link) {
-                    linkStrings.add(link.data.from + " --> " + link.data.to);
-                });
-                linkStrings.sort(function (a, b) { return a < b ? -1 : 1 });
-                linkStrings.each(function (str) {
-                    text += str + ", ";
-                });
-                text = text.substring(0, text.length - 2);
-            }
-        }
-    } else if (x instanceof go.Link) {
-        // if it"s a link, display its to and from nodes
-        text += "Link: " + x.data.from + " --> " + x.data.to +
-            "\nNode To: " + x.data.to + "\nNode From: " + x.data.from;
-        var grp = containing(x, 0); // and containing group, if it has one
-        if (grp !== null) text += "\nContaining SubGraph: " + grp.data.key;
-    }
-    return text;
+    var data = obj.panel.adornedPart.data;
+    if (data.text)
+        return data.text;
+    return data.from + " --> " + data.to;
 }
